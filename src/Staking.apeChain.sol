041186479ab1e55ab7d9dd6dd30b7eddd8d19ad8
ArturVargas <arturvargas9226@gmail.com>
2024-07-13 15:52:41 -0600

    add staking apechain mock

--- src/Staking.apeChain.sol
+++ src/Staking.apeChain.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 // POF (Proof of Financial)
 // staking: APE, Nouns, ETH
+// integrate layer zero to call from FHE
 pragma solidity ^0.8.13;
 
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "fhenix-contracts/contracts/FHE.sol";
 
 contract Staking {
 
@@ -35,4 +35,7 @@ contract Staking {
         ethStakedAmount[msg.sender] += msg.value;
     }
 
+    function getUserBalance(address _user) public view returns (uint, uint, uint) {
+        return (apeStakedAmount[_user], nounsStakedAmount[_user], ethStakedAmount[_user]);
+    }
 }
