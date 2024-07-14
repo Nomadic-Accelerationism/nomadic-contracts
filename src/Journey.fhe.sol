5e04c9ca4abd71d25e63f7c51fa3f1582fe70ee1
ArturVargas <arturvargas9226@gmail.com>
2024-07-13 19:47:20 -0600

    compile and deploy fhe contracts

---
+++ src/Journey.fhe.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// Deployed at: 0x950650fda9c97c24aa90c6f0c3e8d9ddba4a48fb
+pragma solidity ^0.8.13;
+
+import "fhenix-contracts/contracts/FHE.sol";
+
+contract Journey {
+    // check if we can encrypt this...
+    struct NewJourney {
+       eaddress user;
+       euint256 budget;
+       string city;
+       euint256 startDate;
+       euint256 endDate;
+    }
+
+    mapping(eaddress => NewJourney) public hackerJourney;
+
+    function createJourney(
+        address _user,
+        uint256 _budget,
+        string memory _city,
+        uint _startDate,
+        uint _endDate
+    ) public {
+        eaddress eUser = FHE.asEaddress(_user);
+    
+        NewJourney memory newJourney = NewJourney({
+            user: eUser,
+            budget: FHE.asEuint256(_budget),
+            city: _city,
+            startDate: FHE.asEuint256(_startDate),
+            endDate: FHE.asEuint256(_endDate)
+        });
+        hackerJourney[eUser] = newJourney;
+    }
+
+    function getJourney(address _user) public view returns (NewJourney memory) {
+        return hackerJourney[FHE.asEaddress(_user)];
+    }
+}
--- src/Users.fhe.sol
+++ src/Users.fhe.sol
@@ -1,8 +1,46 @@
 // SPDX-License-Identifier: MIT
+// Deployed at: 0x2A19790B6Dd1fC70e45e6F0D64A1a61C79a5Da0c
 pragma solidity ^0.8.13;
 
 import "fhenix-contracts/contracts/FHE.sol";
 
 contract Users {
 
+    // check if we can encrypt this...
+    struct NewHacker {
+       eaddress owner;
+       euint32 age;
+        bytes32 poap;
+        bytes32 nouns;
+        bytes32 ape;
+        bytes32 talent;
+        bytes32 financial;
+        bytes32 identity;
+        bytes32 farcaster;
+        bytes32 ens;
+    }
+
+    mapping(eaddress => NewHacker) public hackers;
+
+    function createHacker(address _owner, uint32 _age, bytes32 _poap, bytes32 _nouns, bytes32 _ape, bytes32 _talent, bytes32 _financial, bytes32 _identity, bytes32 _farcaster, bytes32 _ens) public {
+        eaddress eOwner = FHE.asEaddress(_owner);
+    
+        NewHacker memory newHacker = NewHacker({
+            owner: eOwner,
+            age: FHE.asEuint32(_age),
+            poap: _poap,
+            nouns: _nouns,
+            ape: _ape,
+            talent: _talent,
+            financial: _financial,
+            identity: _identity,
+            farcaster: _farcaster,
+            ens: _ens
+        });
+        hackers[eOwner] = newHacker;
+    }
+
+    function getHacker(address _owner) public view returns (NewHacker memory) {
+        return hackers[FHE.asEaddress(_owner)];
+    }
 }
